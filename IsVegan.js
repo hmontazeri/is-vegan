e8a20f34f04fa3b6ecd5fab39f31f5ce42a52542
Hamed Montazeri <hamed.montazeri@mhp.com>
2018-02-03 21:54:09 -0500

    IsVegan module added

---
+++ IsVegan.js
@@ -0,0 +1,11 @@
+/**
+ * This functions takes the given ingredient
+ * and checks it against the black list of ingredients
+ * @param ingredientToCheck - the ingredient to check
+ */
+export default function isVegan(ingredientToCheck = '') {
+  // lower case it to match black list
+  ingredientToCheck = ingredientToCheck.toLowerCase();
+
+  //TODO: load list and check it against it
+}
