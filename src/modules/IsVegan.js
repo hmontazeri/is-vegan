718c6ce3502d06e7e9a9f9429bd0a29ca7c0e849
Hamed Montazeri <hamed.montazeri@mhp.com>
2018-02-04 00:12:02 -0500

    jsdoc update

--- src/modules/IsVegan.js
+++ src/modules/IsVegan.js
@@ -23,6 +23,7 @@ export function isVeganIngredient(ingredientToCheck = '') {
  * This functions takes a given list of ingredients
  * and checks them against the black list of ingredients
  * @param {string} ingredientsToCheck - the lit of ingredients to check
+ * @return <code>true</code> if ingredients are not on blacklist
  */
 export function isVeganIngredientList(ingredientsToCheck = []) {
   // jump out if input is empty
