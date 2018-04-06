9f7463ba8de2b00866350e84dbcb8bd2fb156fe6
Hamed Montazeri <hamed.mon@gmail.com>
2018-04-06 08:35:40 -0400

    Update IsVegan.js

--- src/modules/IsVegan.js
+++ src/modules/IsVegan.js
@@ -2,9 +2,9 @@ import nonVeganList from '../util/nonvegan.json';
 
 /**
  * This functions takes the given ingredient
- * and checks it against the black list of ingredients
+ * and checks it against the non-vegan list of ingredients
  * @param {string} ingredientToCheck - the ingredient to check
- * @return <code>true</code> if ingredient is not on nonVeganList
+ * @return <code>true</code> if ingredient is not in nonVeganList
  */
 export function isVeganIngredient (ingredientToCheck) {
   // true if empty
@@ -17,9 +17,9 @@ export function isVeganIngredient (ingredientToCheck) {
 
 /**
  * This functions takes a given list of ingredients
- * and checks them against the black list of ingredients
+ * and checks them against the non-vegan list of ingredients
  * @param {[string]} ingredientsToCheck - the lit of ingredients to check
- * @return <code>true</code> if ingredients are not on nonVeganList
+ * @return <code>true</code> if ingredients are not in nonVeganList
  */
 export function isVeganIngredientList (ingredientsToCheck) {
   return !ingredientsToCheck.some(ingredient => !isVeganIngredient(ingredient));
@@ -27,9 +27,9 @@ export function isVeganIngredientList (ingredientsToCheck) {
 
 /**
  * This functions takes a given list of ingredients
- * and checks them against the black list of ingredients
+ * and checks them against the non-vegan list of ingredients
  * @param {[string]} ingredientsToCheck - the lit of ingredients to check
- * @return Array of ingredients that are non vegan
+ * @return Array of ingredients that are non-vegan
  */
 export function containsNonVeganIngredients (ingredientsToCheck) {
   return ingredientsToCheck.filter(
