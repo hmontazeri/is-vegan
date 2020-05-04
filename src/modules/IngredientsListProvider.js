fade407db0038502aaf1e9c0fbb4d44f6d371cea
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2020-05-04 18:24:35 +0200

    Added functions to get and set the selected language of the ingredients list

--- src/modules/IngredientsListProvider.js
+++ src/modules/IngredientsListProvider.js
@@ -1,6 +1,44 @@
 import enCanBeVeganList from '../i18n/en/canbevegan.json';
 import enNonVeganList from '../i18n/en/nonvegan.json';
 
+var validate = require('validate.js');
+
+const supportedLanguages = ['en', 'it'];
+var selectedLanguage = supportedLanguages[0];
+
+/**
+ * This functions returns the selected language
+ * @return {string} The ISO 639-1 code of the selected language
+ */
+export function getSelectedLanguage () {
+  return selectedLanguage;
+}
+
+/**
+ * This functions sets the selected language
+ * @param {string} value - The ISO 639-1 code of the language to select
+ * @return {undefined}
+ */
+export function setSelectedLanguage (value) {
+  const validationConstraints = {
+    language: {
+      presence: true,
+      inclusion: {
+        within: supportedLanguages,
+        message: '^Language \'%{value}\' is currently not supported'
+      }
+    }
+  };
+
+  const validationResult = validate({language: value}, validationConstraints);
+  if (validationResult !== undefined) {
+    console.log(validationResult);
+    return;
+  }
+
+  selectedLanguage = value;
+}
+
 /**
  * This functions returns the can-be-vegan list of ingredients
  * @return The can-be-vegan list of ingredients
--- test/IngredientsListProvider.test.js
+++ test/IngredientsListProvider.test.js
@@ -1,5 +1,33 @@
 import * as ingredientsListProvider from '../src/modules/IngredientsListProvider';
 
+afterEach(() => {
+  ingredientsListProvider.setSelectedLanguage('en');
+});
+
+test('should return \'en\' as the selected language by default', () => {
+  expect(ingredientsListProvider.getSelectedLanguage()).toEqual('en');
+});
+
+test('should allow selecting a supported language', () => {
+  ingredientsListProvider.setSelectedLanguage('it');
+  expect(ingredientsListProvider.getSelectedLanguage()).toEqual('it');
+});
+
+test('should NOT allow selecting a null language', () => {
+  ingredientsListProvider.setSelectedLanguage(null);
+  expect(ingredientsListProvider.getSelectedLanguage()).toEqual('en');
+});
+
+test('should NOT allow selecting an undefined language', () => {
+  ingredientsListProvider.setSelectedLanguage(undefined);
+  expect(ingredientsListProvider.getSelectedLanguage()).toEqual('en');
+});
+
+test('should NOT allow selecting an unsupported language', () => {
+  ingredientsListProvider.setSelectedLanguage('fr');
+  expect(ingredientsListProvider.getSelectedLanguage()).toEqual('en');
+});
+
 test('should return list of can-be-vegan ingredients', () => {
   expect(ingredientsListProvider.getCanBeVeganList()).toEqual(expect.arrayContaining(['biotin', 'vitamin b12']));
 });
